100350cdd607d56e1d4717f266401c0a6d34dc99
SakshiAg02 <113249274+SakshiAg02@users.noreply.github.com>
2023-02-26 15:44:40 +0530

    Add files via upload

--- obj_trk_final.py
+++ obj_trk_final.py
@@ -8,7 +8,7 @@ p2 = 300
 xs = []
 ys = []
 
-video = cv2.VideoCapture("footvolleyball.mp4")
+video = cv2.VideoCapture("ball_video1.mp4")
 #load tracker 
 tracker = cv2.TrackerCSRT_create()
 
@@ -46,28 +46,24 @@ def drawBox(img,bbox):
     cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
 
 
-
-def drawBox(img,bbox):
-    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
-    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
-    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
-    pass
-
-
 while True:
-   check,img = video.read()
-   success,bbox = teacker.update(img)
-   pass
+    check,img = video.read()   
+    success,bbox = tracker.update(img)
+
+    if success:
+        drawBox(img,bbox)
+    else:
+        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+
+
+    goal_track(img,bbox)
+
+    cv2.imshow("result",img)
+            
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        print("Closing")
+        break
+
 video.release()
-cv2.destroyALLwindows()
-
-if success:
-    drawBoz(img,bbox)
-else:
-    cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-
-key = cv2.waitKey(1)
-if key == ord('q'):
-    print("Closing")
-    break
-
+cv2.destroyALLwindows() 
